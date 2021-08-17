b80b8af8d92e905697ca94b4fdb2faa2c3451e28
phpunch <nutacp_ryho@hotmail.com>
2021-08-17 16:48:56 +0700

    Create custom error

---
+++ customError/customError.go
@@ -0,0 +1,19 @@
+package customError
+
+type ValidationError struct {
+	Code    uint64 `json:"code"`
+	Message string `json:"message"`
+}
+
+func (e *ValidationError) Error() string {
+	return e.Message
+}
+
+type InternalError struct {
+	Code    uint64 `json:"code"`
+	Message string `json:"message"`
+}
+
+func (e *InternalError) Error() string {
+	return e.Message
+}
