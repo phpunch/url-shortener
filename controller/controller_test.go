004e7eb81d43fcbd7d79d664b0f3726c19280208
phpunch <nutacp_ryho@hotmail.com>
2021-08-20 22:11:20 +0700

    Update test case

--- controller/controller_test.go
+++ controller/controller_test.go
@@ -6,27 +6,14 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/assert"
+	"gotest.tools/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 	"url-shortener/mock"
+	"url-shortener/model"
 )
-
-func setupRouter(t *testing.T) *gin.Engine {
-	mockCtrl := gomock.NewController(t)
-	serv := mock.NewMockService(mockCtrl)
-	ctrl := New(serv)
-
-	router := gin.Default()
-	router.POST("/shorten", ctrl.Shorten)
-	router.GET("/:shortCode", ctrl.Redirect)
-	router.GET("/admin/urls", ctrl.GetUrls)
-	router.DELETE("/:shortCode", ctrl.DeleteUrl)
-	router.Run(":8080")
-
-	return router
-}
 
 func TestShortenRoute(t *testing.T) {
 	gin.SetMode(gin.TestMode)
@@ -52,7 +39,7 @@ func TestShortenRoute(t *testing.T) {
 	c.Request, _ = http.NewRequest("POST", "/shorten", bytes.NewReader(jsonBytes))
 	router.ServeHTTP(w, c.Request)
 
-	var resp Response
+	var resp model.Response
 	if err := json.NewDecoder(w.Body).Decode(&resp); err != nil {
 		t.Fatalf("failed to decode, err: %v", err)
 	}
@@ -82,8 +69,65 @@ func TestRedirectRoute(t *testing.T) {
 	router.ServeHTTP(w, c.Request)
 
 	t.Log(w.HeaderMap.Get("Location"))
-	assert.Equal(t, http.StatusMovedPermanently, w.Code)
+	assert.Equal(t, http.StatusFound, w.Code)
 	assert.Equal(t, output, w.HeaderMap.Get("Location"))
+}
+func TestGetUrlsRoute(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	c, router := gin.CreateTestContext(httptest.NewRecorder())
+	serv := mock.NewMockService(mockCtrl)
+	ctrl := New(serv)
+
+	mockedTime, _ := time.Parse(time.RFC3339Nano, "2021-08-20T22:06:32.6162088+07:00")
+	output := []*model.UrlObject{
+		&model.UrlObject{
+			FullURL:   "http://www.facebook.com",
+			ShortCode: "7XxYzjImrg6",
+			Expiry:    &mockedTime,
+			Hits:      2,
+		},
+		&model.UrlObject{
+			FullURL:   "http://www.netflix.com",
+			ShortCode: "4oEQByEsvg4",
+			Hits:      25,
+		},
+	}
+	expected := []interface{}{
+		map[string]interface{}{
+			"expiry":    "2021-08-20T22:06:32.6162088+07:00",
+			"fullUrl":   "http://www.facebook.com",
+			"hits":      float64(2),
+			"shortCode": "7XxYzjImrg6",
+		},
+		map[string]interface{}{
+			"fullUrl":   "http://www.netflix.com",
+			"hits":      float64(25),
+			"shortCode": "4oEQByEsvg4",
+		},
+	}
+	serv.EXPECT().
+		GetUrlObjects(gomock.Any(), nil, nil).
+		Return(output, nil)
+
+	router.GET("/admin/urls", ctrl.GetUrls)
+
+	w := httptest.NewRecorder()
+
+	// completeUrl := fmt.Sprintf("/admin/urls?shortCode=%s&fullUrl=%s", shortCode, fullUrl)
+	c.Request, _ = http.NewRequest("GET", "/admin/urls", nil)
+	c.Request.Header.Set("Token", adminToken)
+	router.ServeHTTP(w, c.Request)
+
+	var resp model.Response
+	if err := json.NewDecoder(w.Body).Decode(&resp); err != nil {
+		t.Fatalf("failed to decode, err: %v", err)
+	}
+	t.Log(resp)
+	assert.Equal(t, 200, w.Code)
+	assert.DeepEqual(t, expected, resp.Data)
 }
 func TestDeleteUrlRoute(t *testing.T) {
 	gin.SetMode(gin.TestMode)
